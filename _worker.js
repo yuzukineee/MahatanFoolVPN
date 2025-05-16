1bd7eecd512ecad14ea88a9e9cd016990a22e9b7
yuzukineee <mahatanfoolvpn@gmail.com>
2025-05-16 23:35:49 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -118,7 +118,7 @@ function getAllConfig(request, hostName, proxyList, page = 0) {
 
     // Build HTML
     const document = new Document(request);
-    document.setTitle("Welcome to <span class='text-blue-500 font-semibold'>Nautica</span>");
+    document.setTitle("Welcome to <span class='text-blue-500 font-semibold'>MahatanFoolVPN</span>");
     document.addInfo(`Total: ${proxyList.length}`);
     document.addInfo(`Page: ${page}/${Math.floor(proxyList.length / PROXY_PER_PAGE)}`);
 
